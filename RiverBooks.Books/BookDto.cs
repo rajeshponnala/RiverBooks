52f3028381ce3f1e7eb5818e0a8f32be93f1a14d
rajeshponnala <rajeshreddyponnala@gmail.com>
2025-03-01 23:40:45 +0530

    Add Domain Model
    - Add Book entity
    
    - Use Guard Clause Library to validate the domain model data

--- RiverBooks.Books/BookDto.cs
+++ RiverBooks.Books/BookDto.cs
@@ -1,4 +1,27 @@
-﻿namespace RiverBooks.Books;
+﻿using System.Data;
+using Ardalis.GuardClauses;
+
+namespace RiverBooks.Books;
 
 public record BookDto(Guid Id, string Title, string Author);
 
+internal class Book
+{
+   public Guid Id { get; set; }
+  public string Title { get; set; }
+  public string Author { get; set; }
+  public decimal Price { get; set; }
+
+  internal Book(Guid id, string title, string author, decimal price)
+  {
+    Id =  Guard.Against.Default(id);
+    Title = Guard.Against.NullOrEmpty(title);
+    Author = Guard.Against.NullOrEmpty(author);
+    Price = Guard.Against.Negative(price);
+  }
+
+  internal void UpdatePrice(decimal newPrice)
+  {
+    Price = Guard.Against.Negative(newPrice);
+  }
+}
