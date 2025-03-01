085990fc1cb2a6a831a7823b0add4daae657c52d
rajeshponnala <rajeshreddyponnala@gmail.com>
2025-03-01 22:35:48 +0530

    Refactor the code

---
+++ RiverBooks.Books/BookDto.cs
@@ -0,0 +1,4 @@
+﻿namespace RiverBooks.Books;
+
+public record BookDto(Guid Id, string Title, string Author);
+
---
+++ RiverBooks.Books/BookEndPoints.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace RiverBooks.Books;
+
+public static class BookEndPoints
+{
+    public static string GetBooks = "/books";
+    public static string GetBookById = "api/books/{id}";
+    public static string CreateBook = "api/books";
+    public static string UpdateBook = "api/books/{id}";
+    public static string DeleteBook = "api/books/{id}";
+    
+    public static void MapBookEndPoints(this WebApplication app)
+    {
+        app.MapGet(GetBooks, (IBookService bookService) => bookService.ListBooks());
+    } 
+
+}
+
